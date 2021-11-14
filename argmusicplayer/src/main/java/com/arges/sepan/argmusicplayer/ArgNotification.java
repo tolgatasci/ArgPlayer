ce6fa98b792dba6d512fe34c0d06b41e90f52b45
Tolga <tolgatasci1@gmail.com>
2021-11-14 20:44:50 +0300

    Update ArgNotification.java
    notification not displayed. After some research it got fixed. Better check and edit the codes. By the way, thanks to you, we can take quick steps.

--- argmusicplayer/src/main/java/com/arges/sepan/argmusicplayer/ArgNotification.java
+++ argmusicplayer/src/main/java/com/arges/sepan/argmusicplayer/ArgNotification.java
@@ -1,17 +1,25 @@
 package com.arges.sepan.argmusicplayer;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.widget.RemoteViews;
 
 import com.arges.sepan.argmusicplayer.IndependentClasses.Arg;
 
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 @SuppressLint("ParcelCreator")
 public class ArgNotification extends Notification {
@@ -22,13 +30,47 @@ public class ArgNotification extends Notification {
     private static boolean notif = false;
     private static boolean progress = false;
     protected static Class mainActivityClass;
-    protected static int notifImgResId = R.drawable.mergesoftlogo;
+    protected static int notifImgResId = R.drawable.mergesoft;
 
-    @SuppressLint("NewApi")
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public ArgNotification(Context context, String audio, int duration){
         super();
-        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notification = new Builder(context).setWhen(System.currentTimeMillis()).setTicker("ArgPlayer").setSmallIcon(notifImgResId).build();
+        String ns = Context.NOTIFICATION_SERVICE;
+        String NOTIFICATION_CHANNEL_ID = "548853";
+        mNotificationManager = (NotificationManager) context.getSystemService(ns);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+        notification = builder
+                .setContentTitle("arg")
+                .setWhen(System.currentTimeMillis())
+                .setTicker("ArgPlayer")
+                .setSmallIcon(notifImgResId)
+                .build();
+
+       /* notification = new Builder(context)
+                .setWhen(System.currentTimeMillis()).setTicker("ArgPlayer").setSmallIcon(notifImgResId).build();*/
+
+
+
+
+
+
 
         contentView=new RemoteViews(context.getPackageName(), R.layout.notification_layout);
         Intent homeIntent = null;
@@ -63,7 +105,9 @@ public class ArgNotification extends Notification {
             bigContentView.setOnClickPendingIntent(R.id.btnCloseBigNotif, PendingIntent.getBroadcast(context,8,closeIntent,PendingIntent.FLAG_CANCEL_CURRENT));
             bigContentView.setInt(R.id.btnPlayPauseBigNotif, "setImageResource", R.drawable.arg_notif_pause);
             bigContentView.setTextViewText(R.id.tvAudioNameBigNotif, audio);
+
             notification.bigContentView = bigContentView;
+
         }else{
             contentView.setInt(R.id.relLayProgressNotif,"setVisibility", GONE);
         }
